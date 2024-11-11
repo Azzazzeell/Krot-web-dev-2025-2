2a3c2c2b9878a207082b6f2bdd1f9964952f1dfd
Azzazzeell <146361334+Azzazzeell@users.noreply.github.com>
2024-11-12 01:28:32 +0300

    Add files via upload

---
+++ Lab№4/dishes.js
@@ -0,0 +1,74 @@
+const dishes = [
+    {
+        keyword: 'gaspacho',
+        name: 'Гаспачо',
+        price: 195,
+        category: 'soup',
+        count: '350 г',
+        image: 'dish1'
+    },
+    {
+        keyword: 'mushroom_soup',
+        name: 'Грибной суп-пюре',
+        price: 185,
+        category: 'soup',
+        count: '330 г',
+        image: 'dish2'
+    },
+    {
+        keyword: 'norwegian_soup',
+        name: 'Норвежский суп',
+        price: 270,
+        category: 'soup',
+        count: '330 г',
+        image: 'dish3'
+    },
+    {
+        keyword: 'fried_potatoes',
+        name: 'Жареная картошка с грибами',
+        price: 150,
+        category: 'main_dish',
+        count: '250 г',
+        image: 'dish4'
+    },
+    {
+        keyword: 'lasagna',
+        name: 'Лазанья',
+        price: 385,
+        category: 'main_dish',
+        count: '310 г',
+        image: 'dish5'
+    },
+    {
+        keyword: 'chicken_cutlets',
+        name: 'Котлеты из курицы с картофельным пюре',
+        price: 225,
+        category: 'main_dish',
+        count: '280 г',
+        image: 'dish6'
+    },
+    {
+        keyword: 'orange_juice',
+        name: 'Апельсиновый сок',
+        price: 120,
+        category: 'drink',
+        count: '300 мл',
+        image: 'dish7'
+    },
+    {
+        keyword: 'apple_juice',
+        name: 'Яблочный сок',
+        price: 90,
+        category: 'drink',
+        count: '300 мл',
+        image: 'dish8'
+    },
+    {
+        keyword: 'carrot_juice',
+        name: 'Морковный сок',
+        price: 110,
+        category: 'drink',
+        count: '300 мл',
+        image: 'dish9'
+    }
+];
